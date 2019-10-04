eba2c4b2634c0db3b648f6e18daac67b494f72bc
donk3665 <55457782+donk3665@users.noreply.github.com>
2019-10-04 15:09:26 -0400

    Update ObjectHeight.java

--- ICS3U - Richard Kha/src/kha/unit1/ObjectHeight.java
+++ ICS3U - Richard Kha/src/kha/unit1/ObjectHeight.java
@@ -12,19 +12,37 @@ public class ObjectHeight {
 	 * 
 	 * This is the entry point to the program
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 
 		Scanner sc = new Scanner(System.in);
-
+		double time = 0;
+		int i=0;
 		System.out.println("Enter a value for time less than 4.5 seconds");
+		
+		
+		//this loop prevents bad input from the user, as in input not under 4.5 or strings
+		while (i ==0) {
+			i=1;
+			try {
+				time = Double.parseDouble(sc.nextLine());
+				if (time>=4.5) {
+					i=0;
+					System.out.println("Input is not under 4.5, try again");
+				}
+			}
 
-		double time = sc.nextDouble();
+			catch (Exception e) {
+				System.out.println("Incorrect input, please try again");
+				i=0;
+			}
 
+		}
 		double objectHeight = 100-2.9*Math.pow(time, 2);
 
 		System.out.println("The height of the object is: "+ objectHeight);
 
 
 	}
+
 
 }
