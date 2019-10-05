1df99da7b5acc32e49b31967d696e5eeccb317d7
donk3665 <55457782+donk3665@users.noreply.github.com>
2019-10-04 20:17:11 -0400

    Update Digits.java

--- ICS3U - Richard Kha/src/kha/unit1/Digits.java
+++ ICS3U - Richard Kha/src/kha/unit1/Digits.java
@@ -13,23 +13,42 @@ public class Digits {
 	 * This is the entry point to the program
 	 */
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 
 		//prompts the user to input a number and stores it in 'digits'
-		System.out.println("Give a integer from 0 and 999");
-		int digits = sc.nextInt();
+	
+		int digits = 0;
+		boolean inputValid = false;
+		
+		//this loop ends when the user enters valid input
+		while (inputValid == false) {
+			inputValid = true;
+			System.out.println("Give a integer from 0 and 999");
+			//this try catch catches bad input such as symbols and numbers which are not between 0 and 999
+			try {
+				digits = Integer.parseInt(sc.nextLine());
+				if (digits<0 || digits>999) {
+					inputValid = false;
+					System.out.println("Input is not between the range of 0 and 999");
+				}
+			}
+			catch (Exception e) {
+				System.out.println("Incorrect input, please try again");
+				inputValid = false;
+			}
 
+		}
 		//performs calculations to find the value of the ones, tens and hundreds of a number
 		int hundreds = digits/100;
 		digits -= hundreds*100;
 		int tens = digits/10;
 		digits -= tens*10;
-
-		//digits is ones due to the calculations
+		//due to calculations, digits becomes the value for ones at the end
+		
 		System.out.println("Hundreds: "+ hundreds );
 		System.out.println("Tens: "+ tens );
 		System.out.println("Ones: "+ digits);
+
 
 
 	}
