eca955b26e204ad7cf60592360571a50c5c01eb4
donk3665 <55457782+donk3665@users.noreply.github.com>
2019-10-04 20:06:54 -0400

    Update Quadratic.java

--- ICS3U - Richard Kha/src/kha/unit1/Quadratic.java
+++ ICS3U - Richard Kha/src/kha/unit1/Quadratic.java
@@ -13,22 +13,43 @@ public class Quadratic {
 	 * This is the entry point to the program
 	 */
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 
-		//This section prompts the user for values for the variables a, b and c
-		System.out.println("Please give me a value for a");
-		double aValue = sc.nextDouble();
-		System.out.println("Please give me a value for b");
-		double bValue = sc.nextDouble();
-		System.out.println("Please give me a value for c");
-		double cValue = sc.nextDouble();
+		//prompts the user to input a number and stores it in 'digits'
+	
+		int digits = 0;
+		boolean inputValid = false;
+		
+		//this loop ends when the user enters valid input
+		while (inputValid == false) {
+			inputValid = true;
+			System.out.println("Give a integer from 0 and 999");
+			//this try catch catches bad input such as symbols and numbers which are not between 0 and 999
+			try {
+				digits = Integer.parseInt(sc.nextLine());
+				if (digits<0 || digits>999) {
+					inputValid = false;
+					System.out.println("Input is not between the range of 0 and 999");
+				}
+			}
+			catch (Exception e) {
+				System.out.println("Incorrect input, please try again");
+				inputValid = false;
+			}
 
-		//This section performs the quadratic equation with the three variables, finding both of the roots
-		double quadraticAnswer1 = (-bValue+Math.sqrt(Math.pow(bValue, 2)- 4* aValue*cValue))/(2*aValue);
-		double quadraticAnswer2 = (-bValue-Math.sqrt(Math.pow(bValue, 2)- 4* aValue*cValue))/(2*aValue);
+		}
+		//performs calculations to find the value of the ones, tens and hundreds of a number
+		int hundreds = digits/100;
+		digits -= hundreds*100;
+		int tens = digits/10;
+		digits -= tens*10;
 
-		System.out.println("Your roots are: "+ quadraticAnswer1+ ", and "+ quadraticAnswer2 );
+		//digits is ones due to the calculations
+		System.out.println("Hundreds: "+ hundreds );
+		System.out.println("Tens: "+ tens );
+		System.out.println("Ones: "+ digits);
+
+
 
 	}
 
