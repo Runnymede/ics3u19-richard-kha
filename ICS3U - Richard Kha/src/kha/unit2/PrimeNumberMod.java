c59ab263a801d3614e0e467e248344712ae27627
donk3665 <55457782+donk3665@users.noreply.github.com>
2019-11-05 14:48:53 -0500

    Comments and additional Javadoc

--- ICS3U - Richard Kha/src/kha/unit2/PrimeNumberMod.java
+++ ICS3U - Richard Kha/src/kha/unit2/PrimeNumberMod.java
@@ -17,34 +17,41 @@ public class PrimeNumberMod {
 		int number1 = inputParse("1st");
 		int number2 = inputParse("2nd");
 		int numberStorage;
+		int counter;
+		boolean prime;
+		//if statement allows for user to put in the numbers in any order (e.g. lowest to highest and highest to lowest)
 		if (number2< number1) {
 			numberStorage = number1;
 			number1 = number2;
 			number2 = numberStorage;
 		}
-		int counter;
-		boolean prime;
+		
+		//while statement tests all the numbers in between the two inputed (inclusive) for if they are prime numbers
 		while (number1<=number2) {
 			prime = true;
 			counter = 2;
-			while (counter<=(int)(Math.sqrt(Math.abs(number1)))){
+			//this while loop specifically tests each individual number
+			while (counter<=(int)Math.sqrt(Math.abs(number1))){
 				if (number1%counter == 0) {
 					prime = false;
 					break;
 				}
-
 				counter++;
 			}
+			//if statement deals with the case that the user inputs Integer.MIN_VALUE
 			if (number1 == -2147483648) {
 				prime = false;
 			}
 			if (prime == true) {
-
 				System.out.println(number1);
 			}
 			number1++;
 		}
 	}
+	/**
+	   * Gets the users input and refuses bad input.
+	   * @return integer for main calculations.
+	   */
 	static int inputParse(String numberExamined) {
 		Scanner sc = new Scanner(System.in);
 		int integerInput = 0;
