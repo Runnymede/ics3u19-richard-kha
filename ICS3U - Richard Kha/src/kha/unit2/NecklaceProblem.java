eaba7b391e6f73a22d220f35693891ddafd7beaf
donk3665 <55457782+donk3665@users.noreply.github.com>
2019-11-07 14:41:47 -0500

    Update NecklaceProblem.java

--- ICS3U - Richard Kha/src/kha/unit2/NecklaceProblem.java
+++ ICS3U - Richard Kha/src/kha/unit2/NecklaceProblem.java
@@ -56,6 +56,7 @@ public class NecklaceProblem {
 			try {
 				System.out.println("Please give me a integer from 0 to 9 for your "+numberExamined+ " number");
 				integerInput= Integer.parseInt((sc.nextLine()));
+				//prevents input lower than 0 and higher than 9
 				if (integerInput<0 || integerInput>9) {
 					System.out.println("Incorrect input, please try again");
 					inputValid = false;
