687cfe941a040a699ae628916ae05f79e1cf93d4
Richard Kha <55457782+donk3665@users.noreply.github.com>
2019-11-25 22:12:14 -0500

    Commenting

--- ICS3U - Richard Kha/src/kha/unit3/DiceRolls.java
+++ ICS3U - Richard Kha/src/kha/unit3/DiceRolls.java
@@ -3,13 +3,24 @@ package kha.unit3;
 public class DiceRolls {
 
 	public static void main(String[] args) {
+		//declaring and assigning variables
 		int[] numberOfRolls = new int[11];
+		int dice1 = 0;
+		int dice2 = 0;
+		int total = 0;
+		
+		//loop to perform opperations 1000 times 
 		for (int a = 0; a<1000; a++) {
-			int dice1 = (int)(Math.random()*6+1);
-			int dice2 = (int)(Math.random()*6+1);
-			int total = dice1+dice2;
+			
+			//generating two random numbers and then added them together
+			dice1 = (int)(Math.random()*6+1);
+			dice2 = (int)(Math.random()*6+1);
+			total = dice1+dice2;
+			
+			//counting occurances of each number from 2 to 12
 			numberOfRolls[total-2] ++;
 		}
+		//displaying the results from the operations in a neat table-like fashion
 		System.out.println("Total\t Number of Rolls");
 		for (int i = 0; i<11; i++) {
 			System.out.println(i+2+"\t "+numberOfRolls[i]);
