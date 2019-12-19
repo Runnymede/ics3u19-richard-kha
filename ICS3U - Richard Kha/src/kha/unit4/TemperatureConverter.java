b7c45329954a46207bb4e75f7fdf53374ac17a08
333599769 <333599769@C-2DF4TJ1.tdsb.on.ca>
2019-12-19 13:58:18 -0500

    Commenting

--- ICS3U - Richard Kha/src/kha/unit4/TemperatureConverter.java
+++ ICS3U - Richard Kha/src/kha/unit4/TemperatureConverter.java
@@ -14,65 +14,108 @@ public class TemperatureConverter {
 	 * This is the entry point to the program (throws are for BufferedReader)
 	 * @throws IOException
 	 */
-	public static void main(String[] args) throws IOException {
-		//declaring variables
+public static void main(String[] args) throws IOException {	
+		//declaring and assigning variables and objects
 		BufferedReader sc =  new BufferedReader(new InputStreamReader(System.in));
+		String input;
+		double value;
+		boolean inputValid;
+		boolean pass;
+		//do while loop wraps around entire program, allowing user to do multiple calculations in one session		
 		do {
-		String input = " ";
-		double value = 0;
-		boolean inputValid = false;
-		boolean pass = false;
-		System.out.println("Would you like to convert Celsius to Fahrenheit or Fahrenheit to Celsius?");
 
-		do  {
 
-			System.out.println("Enter 1 for Celsius to Fahrenheit");
-			System.out.println("Enter 2 for Fahrenheit to Celsius");
-			input = sc.readLine().trim();
 
-			pass = !input.equals("1")&&!input.equals("2");
-			if (pass == true) {
-				System.out.println("Invalid input, please try again");
+			input = " ";
+			value = 0;
+			inputValid = false;
+			pass = false;
+
+
+			System.out.println("Would you like to convert Celsius to Fahrenheit or Fahrenheit to Celsius?");
+
+			//this loop ends when the user enters valid input
+			do  {
+
+
+
+				System.out.println("Enter 1 for Celsius to Fahrenheit");
+				System.out.println("Enter 2 for Fahrenheit to Celsius");
+				input = sc.readLine().trim();
+
+				//statement making sure input is valid
+				pass = !input.equals("1")&&!input.equals("2");
+
+				if (pass == true) {
+					System.out.println("Invalid input, please try again");
+				}
+
+
+
+			}while (pass == true);
+
+
+
+
+
+
+			//this loop ends when the user enters valid input
+			while (inputValid == false) {
+				inputValid = true;
+				System.out.println("Input the number you want to convert");
+
+
+
+				//this try catch catches bad input such as symbols 
+				try {
+					value = Double.parseDouble(sc.readLine());
+				}
+				catch (Exception e) {
+					System.out.println("Invalid input, please try again");
+					inputValid = false;
+				}
+
+
+
 			}
 
-		}while (pass == true);
+			//if else calls on one of the two methods, based on the user’s input
+			if (input.equals("1")) {
 
-
-		//this loop ends when the user enters valid input
-		while (inputValid == false) {
-			inputValid = true;
-			System.out.println("Input the number you want to convert");
-
-			//this try catch catches bad input such as symbols and numbers which are not over or equal to 0
-			try {
-				value = Double.parseDouble(sc.readLine());
+				System.out.println("Your answer is "+cToF(value)+"°C");
 			}
-			catch (Exception e) {
-				System.out.println("Invalid input, please try again");
-				inputValid = false;
+			else {
+
+				System.out.println("Your answer is "+fToC(value)+"°F");
 			}
+			System.out.println("Enter 1 if you want to do another calculation");
 
-		}
 
-		if (input.equals("1")) {
+		} while (sc.readLine().trim().equals("1"));
 
-			System.out.println("Your answer is "+cToF(value)+"°C");
-		}
-		else {
-
-			System.out.println("Your answer is "+fToC(value)+"°F");
-		}
-		System.out.println("Enter 1 if you want to do another calculation");
-	} while (sc.readLine().trim().equals("1"));
-		
 	}
+
+	/**
+	 * This method converts a Celsius value to a Fahrenheit value
+	 * @param value
+	 * @return a double used in main method which is then printed to the console
+	 */
 	public static double cToF(double value) {
+		//formula for Celsius to Fahrenheit
 		double answer = (value*9/5)+32;
 		return answer;
 	}
+	/**
+	 * This method converts a Fahrenheit value to a Celsius value
+	 * @param value
+	 * @return a double used in main method which is then printed to the console
+	 */
 	public static double fToC(double value) {
+		//formula for Fahrenheit to Celsius
 		double answer = (value-32)/9*5;
 		return answer;
 	}
+
+
 
 }
